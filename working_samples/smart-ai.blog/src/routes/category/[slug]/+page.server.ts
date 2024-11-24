972e0b3f53a79221c2d872b287be5c4f039c281f
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2024-11-24 11:50:09 +0100

    remove temp working-samples

--- working_samples/smart-ai.blog/src/routes/category/[slug]/+page.server.ts
+++
@@ -1,3 +0,0 @@
-export const csr = false;
-export const ssr = true;
-export const prerender = false; // needs to take publishedAt into account
--- working_samples/smart-ai.blog/src/routes/talk-to-site/+page.server.ts
+++
@@ -1,3 +0,0 @@
-export const csr = true;
-export const ssr = true;
-export const prerender = true;
