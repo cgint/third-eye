3444332c40390780cb324c314e94bd7dd89326a1
Christian Gintenreiter <christian.gintenreiter@gmail.com>
2024-11-24 13:23:07 +0100

    update prompt

--- src/lib/services/imageAnalyzer.ts
+++ src/lib/services/imageAnalyzer.ts
@@ -33,8 +33,11 @@ export class ImageAnalyzer {
                 2. Information regarding the product's nutritional value, including calories, protein, carbohydrates, and fat
                 2. Any relevant product details
                 
+                Start with a very short and easy to grasp table if the items above with yes, no, unknown (add citation if possible).
                 Please be specific and concise in your response.
                 People should get a good idea of what the product is about and if it might be suitable for them.
+
+                It is of highes importance for the user that you use the following language for your answer: German
             `;
 
             const parts: Part[] = [
